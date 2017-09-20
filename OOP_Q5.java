8c09bbe78ad82722d6a2be4f054328ef9c0eb2a0
t00185230 <Christopher.Tearle@Students.ittralee.ie>
2017-09-20 16:42:06 +0100

    Initial Work

---
+++ OOP_Q5.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class OOP_Q5 {
+    public static void main (String args[]){
+
+
+
+
+
+        String Choice = JOptionPane.showInputDialog("Please enter your Choice");
+
+        int Num1 = Integer.parseInt(JOptionPane.showInputDialog("Please enter a Number"));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Please enter another Number"));
+
+
+      switch (Choice){
+        case 1: Choice = "A";
+
+
+    }
+
+
+
+    //  JOptionPane.showMessageDialog(null,"" );
+
+}
+
+
