d4610017e2e192d54c20190f1986a5fed71f767a
t00185230 <Christopher.Tearle@student.ittralee.ie>
2017-09-21 09:19:03 +0100

    Updated

--- OOP_Q5.java
+++ OOP_Q5.java
@@ -1,35 +1,42 @@
 import javax.swing.*;
 
 public class OOP_Q5 {
-    public static void main (String args[]){
+    public static void main(String args[]) {
 
 
-
-
-
-        String Choice = JOptionPane.showInputDialog("Please enter your Choice");
+        String choice = JOptionPane.showInputDialog("Please enter your Choice");
 
         int Num1 = Integer.parseInt(JOptionPane.showInputDialog("Please enter a Number"));
-        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Please enter another Number"));
+        int Num2 = Integer.parseInt(JOptionPane.showInputDialog("Please enter another Number"));
+        double operation =0;
 
 
-      switch (Choice){
-        case 1: Choice = "A";
-            break;
-        case 2: Choice = "S";
-              break;
+        switch (choice) {
+            case "A":
 
-        case 3: Choice = "M";
-              break;
-        case 4: Choice = "D";
-              break;
+
+                operation = (Num1 + Num2);
+                break;
+            case "S":
+
+                operation = (Num1 - Num2);
+                break;
+
+            case "M":
+
+                operation = (Num1 * Num2);
+                break;
+            case "D":
+
+                 operation = (Num1 / Num2);
+                break;
+
+
+
+        }
+        JOptionPane.showMessageDialog(null, " The First Number entered: " + Num1 + " The second Number entered: " + Num2 + " Calculation Solution: " + operation);
 
     }
-
-
-
-    //  JOptionPane.showMessageDialog(null,"" );
-
 }
 
 
