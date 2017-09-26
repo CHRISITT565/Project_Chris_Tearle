1a2db6d95d200511a3192f465582b9b907ae2fb2
t00185230 <Christopher.Tearle@student.ittralee.ie>
2017-09-26 15:59:30 +0100

    Complete

---
+++ BOOK.java
@@ -0,0 +1,60 @@
+public class BOOK {
+    private String title;
+    private int pageNum;
+    private float price;
+    private String isbn;
+
+    public BOOK(){
+        this("unknown","Unknown",0,0.0f);
+    }
+
+    public BOOK(String title, String isbn,int pageNum, Float price)
+    {
+        setTitle("Unknown");
+        setIsbn("Unknown");
+        setPageNum(0);
+        setPrice(0.0f);
+
+    }
+    public void setTitle(String Title)
+    {
+        this.title=Title;
+    }
+
+    public void setIsbn(String Isbn) {
+
+
+        this.isbn = Isbn;
+    }
+
+    public void setPageNum(int PageNum)
+    {
+        this.pageNum=PageNum;
+    }
+
+    public void setPrice(float Price)
+    {
+        this.price=Price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public String toString(){
+        return String.format("Title %20s" +
+                "ISBN %20s ");
+    }
+}
---
+++ OOP_Q5_LAB2.java
@@ -0,0 +1,5 @@
+public class OOP_Q5_LAB2 {
+    public static void main(String[] args) {
+        int x = 6, c = -16,
+    }
+}
