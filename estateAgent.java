7f4ea2ef8066412fc4465466e8e333b32518041b
t00185230 <Christopher.Tearle@students.ittralee.ie>
2017-10-24 15:35:22 +0100

    Problem set 3 q1

---
+++ estateAgent.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class estateAgent {
+    public static void main(String[] args) {
+        House H1 = new House();
+
+        House H2 = new House();
+
+
+        JOptionPane.showMessageDialog(null, "House " +  H1.toString());
+    }
+}
